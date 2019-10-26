4af24b91fa88446de77458bb16f77f001e283f49
hxling <hxl_apple@hotmail.com>
2019-10-26 19:49:35 +0800

    分组行行号与复选框折叠问题

--- projects/datagrid/src/lib/components/body/fixed-left-rows.component.ts
+++ projects/datagrid/src/lib/components/body/fixed-left-rows.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef, ElementRef, Injector, NgZone, Input, Output, EventEmitter } from '@angular/core';
 import { DatagridComponent } from '../../datagrid.component';
 import { DatagridBodyComponent } from './datagrid-body.component';
-import { ROW_INDEX_FIELD, IS_GROUP_ROW_FIELD, GROUP_ROW_FIELD, IS_GROUP_FOOTER_ROW_FIELD } from '../../services/state';
+import { ROW_INDEX_FIELD, IS_GROUP_ROW_FIELD, GROUP_VISIBLE_FIELD, GROUP_ROW_FIELD, IS_GROUP_FOOTER_ROW_FIELD } from '../../services/state';
 import { DatagridFacadeService } from '../../services/datagrid-facade.service';
 
 @Component({
@@ -20,6 +20,7 @@ export class FixedLeftRowsComponent implements OnInit {
     isGroupRow = IS_GROUP_ROW_FIELD;
     groupRow = GROUP_ROW_FIELD;
     isGroupFooter = IS_GROUP_FOOTER_ROW_FIELD;
+    visible = GROUP_VISIBLE_FIELD;
     public dg: DatagridComponent;
     public dgb: DatagridBodyComponent;
 
