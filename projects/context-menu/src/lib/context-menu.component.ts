3f62a062f72f463ff9d1b8c77f6282d720aa2ec7
hxling <hxl_apple@hotmail.com>
2019-11-17 20:27:56 +0800

    context menu init

---
+++ projects/context-menu/src/lib/context-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-context-menu',
+  template: `
+    <p>
+      context-menu works!
+    </p>
+  `,
+  styles: []
+})
+export class ContextMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/context-menu/src/lib/context-menu.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ContextMenuComponent } from './context-menu.component';
+
+
+
+@NgModule({
+  declarations: [ContextMenuComponent],
+  imports: [
+  ],
+  exports: [ContextMenuComponent]
+})
+export class ContextMenuModule { }
